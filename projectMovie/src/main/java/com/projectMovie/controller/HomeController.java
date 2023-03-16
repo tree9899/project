7ff5d470283b59c5f2233b949a1c220df8e1044b
Limjungeun <202108087@itc.ac.kr>
2023-03-17 06:04:09 +0900

    03.17 두번째 밑작업끝

--- projectMovie/src/main/java/com/projectMovie/controller/HomeController.java
+++ projectMovie/src/main/java/com/projectMovie/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -34,6 +35,13 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	@RequestMapping(value="/testForm")
+	public ModelAndView MemberJoingForm() {
+		ModelAndView mav = new ModelAndView();
+		System.out.println("테스트페이지 이동");
+		mav.setViewName("test");
+		return mav;
 	}
 	
 }
--- projectMovie/src/main/webapp/WEB-INF/views/home.jsp
+++ projectMovie/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,7 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <%@ page session="false" %>
 <html>
 <head>
@@ -10,5 +13,6 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<a href="${pageContext.request.contextPath }/testForm">test</a>
 </body>
 </html>
